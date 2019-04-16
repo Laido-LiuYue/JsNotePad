d3192b28e92aa9c9d8a85d99f40b854e47fb3f74
Laido-LiuYue <578201842@qq.com>
2019-04-16 20:58:11 +0800

    commit note-about

---
+++ note-about/note-about.js
@@ -0,0 +1,42 @@
+/* exported $dlgAbout */
+var $dlgAbout = (function() {
+    var DOM = ''
+          + '<div class="notepad-dlg-about">'
+            + '<div class="dialogbox">'
+              + '<div class="titlebar">'
+                + '<p class="title">关于我的辣鸡记事本</p>'
+                + '<span class="btn-cancel">✖</span>'
+              + '</div>'
+              +'<hr>'
+              + '<div class="main">'
+                + '<h1 class="slogan">JSNotepad</h1>'
+                + '<hr>'
+                + '<div class="info">'
+                  + '<p>作者:Laido-LiuYue</p>'
+                  + '<p>QQ：578201842</p>'
+                  + '<p>仓库地址：<a href="https://github.com/Laido-LiuYue/JsNotePad" target="_blank">https://github.com/Laido-LiuYue/JsNotePad</a></p>'
+                + '</div>'
+                + '<button class="btn-ok btn" type="button" value="确定">确定</button>'
+              + '</div>'
+            + '</div>'
+          + '</div>';
+
+  var $dlg = $(DOM),
+      $btnOk = $dlg.find('.btn-ok'),
+      $btnClose = $dlg.find('.btn-cancel'),
+      $titleBar = $dlg.find('titlebar');
+
+  
+  function destoryDlg() { $dlg.remove();  }
+
+  function show(conf) {
+        
+        $('body').append($dlg);
+        $dlg.find('.dialogbox').draggable({handle: $dlg.find('.titlebar')});
+
+        $btnOk.click(destoryDlg);
+        $btnClose.click(destoryDlg);       
+  }
+    return {show: show};
+
+})();
